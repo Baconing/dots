e05cfc3e784ff6437974ff403a6c0033191ef005
Bacon <brendenfreier951@hotmail.com>
2022-12-16 18:44:19 -0500

    really just a starting point for me, not really ready for everyday use too

---
+++ .config/polybar/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+killall polybar
+
+MAINMON="DP-2"
+
+if type "xrandr"; then
+	  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+      if [[ $m = $MAINMON ]]; then
+            MONITOR=$m TRAY=center polybar main &
+      else
+            MONITOR=$m TRAY=none polybar main &
+      fi
+		        done
+		else
+        echo "install xrandr pls"
+			  polybar main &
+fi
