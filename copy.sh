035272b3e46819a1aff009843c0cab53198785c4
Bacon <brendenfreier951@hotmail.com>
2022-12-18 11:16:28 -0500

    add some files lmao (still not finished)

--- copy.sh
+++ copy.sh
@@ -1,1 +1,7 @@
-todo
+cp -r ~/.config/i3 .config/i3
+cp -r ~/.config/polybar .config/polybar
+cp -r ~/.config/dunst .config/dunst
+cp -r ~/.zshrc .zshrc
+cp -r ~/.oh-my-zsh .oh-my-zsh
+cp -r ~/.config/picom.conf .config/picom.conf
+cp -r ~/.config/alacritty .config/alacritty
