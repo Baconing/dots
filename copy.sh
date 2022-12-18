9f177bc512e602b14eb495acad030a5811b7010a
Bacon <brendenfreier951@hotmail.com>
2022-12-18 11:29:59 -0500

    hi

--- copy.sh
+++ copy.sh
@@ -2,6 +2,5 @@ cp -r ~/.config/i3 .config/i3
 cp -r ~/.config/polybar .config/polybar
 cp -r ~/.config/dunst .config/dunst
 cp -r ~/.zshrc .zshrc
-cp -r ~/.oh-my-zsh .oh-my-zsh
 cp -r ~/.config/picom.conf .config/picom.conf
 cp -r ~/.config/alacritty .config/alacritty
