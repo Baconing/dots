ba6e507ebc144405542c0cca96fdbfb63b4468bd
Baconing <iam@baconi.ng>
2025-04-23 13:30:57 +0000

    mix/feat: htop and networkmanager-openvpn

--- home-manager/mixins/apps/console/default.nix
+++ home-manager/mixins/apps/console/default.nix
@@ -13,5 +13,6 @@
       iperf3
       ripgrep
       gnupg
+      htop
     ];
 }
--- nixos/mixins/services/console/networkmanager/default.nix
+++ nixos/mixins/services/console/networkmanager/default.nix
@@ -1,5 +1,6 @@
-{username, ...}:
+{ username, pkgs, ... }:
 {
+   environment.systemPackages = with pkgs; [ networkmanager-openvpn ];
    networking.networkmanager.enable = true;
    users.users.${username}.extraGroups = ["networkmanager"];
 }
