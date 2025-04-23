3ed0a83b97f36b80c82b288dc24e4ee2b7bcf63e
Baconing <iam@baconi.ng>
2025-04-23 12:27:55 +0000

    feat(home/bacon): add git oauth credential helper

--- home-manager/mixins/users/bacon/default.nix
+++ home-manager/mixins/users/bacon/default.nix
@@ -5,8 +5,11 @@
        enable = true;
        userName = "Baconing";
        userEmail = "iam@baconi.ng";
+       extraConfig = {
+         credential.helper = "oauth";
+       }
        signing = {
-         key = "AB9ED05142400E418145041918C8F79B0400D766";
+         key = "AB9ED05142400E418145041918C8F79B0400D766"; # todo: secret or smth?
          signByDefault = true;
        };
      };
