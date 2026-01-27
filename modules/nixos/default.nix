58d451110cce729a6f8eba09196a66152c3ad4bb
Bacon <iam@baconi.ng>
2026-01-26 23:15:54 -0500

    fix: paths

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,11 +1,5 @@
 {
     imports = [
         ./services/kubernetes
-        ./services/jellyfin
-        ./services/metallb
     ];
-
-#     kubernetes = import ./services/kubernetes;
-#     jellyfin = import ./services/jellyfin;
-#     metallb = import ./services/metallb;
 }
