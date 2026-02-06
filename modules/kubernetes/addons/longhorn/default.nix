192d7025cf82010bd1e8266d674c6d7aa0f852f9
Bacon <iam@baconi.ng>
2026-02-06 00:06:38 -0500

    feat: longhorn

--- modules/kubernetes/addons/longhorn/default.nix
+++ modules/kubernetes/addons/longhorn/default.nix
@@ -19,7 +19,7 @@ in {
                 repo = "https://charts.longhorn.io";
                 chart = "longhorn";
                 version = "1.10.1";
-                sha256 = "a871d397e19cf3243949abd41fd294869f5c2c490014f29e71866a2433ec7fb9";
+                sha256 = "nkS4nvFK+K7J/sE+OxOPY0nR3lkrQF5K7JM5zbXLJ0s=";
             };
         };
 
