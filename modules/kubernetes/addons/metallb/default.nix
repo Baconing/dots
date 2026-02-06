33ee962cbe9d324ec5461d00ebd4c66932de9bc9
Bacon <iam@baconi.ng>
2026-02-05 22:53:44 -0500

    ???

--- modules/kubernetes/addons/metallb/default.nix
+++ modules/kubernetes/addons/metallb/default.nix
@@ -12,27 +12,42 @@ in {
     };
 
     config = lib.mkIf cfg.enable {
-        kubernetes.helm.releases.metallb = {
-            chart = kubenix.lib.helm.fetch {
-                repo = "https://metallb.github.io/metallb";
-                chart = "metallb";
-                version = "0.15.3";
-                sha256 = "KWdVaF6CjFjeHQ6HT1WvkI9JnSurt9emLVCpkxma0fg=";
+        kubernetes = { 
+            helm.releases.metallb = {
+                chart = kubenix.lib.helm.fetch {
+                    repo = "https://metallb.github.io/metallb";
+                    chart = "metallb";
+                    version = "0.15.3";
+                    sha256 = "KWdVaF6CjFjeHQ6HT1WvkI9JnSurt9emLVCpkxma0fg=";
+                };
             };
-        };
 
-        kubernetes.customTypes = {
-            ipAddressPool = {
-                attrName = "ipAddressPool";
-                group = "metallb.io";
-                kind = "IPAddressPool";
-                version = "v1beta1";
+            # https://github.com/hall/kubenix/issues/97#issuecomment-3702978320
+            resources = {
+                services.metallb-webhook-service = {
+                    spec = {
+                        ports = lib.mkForce [{
+                            port = 443;
+                            targetPort = 9443;
+                            protocol = "TCP";
+                        }];
+                    };
+                };
             };
-            l2Advertisement = {
-                attrName = "l2Advertisement";
-                group = "metallb.io";
-                kind = "L2Advertisement";
-                version = "v1beta1";
+
+            customTypes = {
+                ipAddressPool = {
+                    attrName = "ipAddressPool";
+                    group = "metallb.io";
+                    kind = "IPAddressPool";
+                    version = "v1beta1";
+                };
+                l2Advertisement = {
+                    attrName = "l2Advertisement";
+                    group = "metallb.io";
+                    kind = "L2Advertisement";
+                    version = "v1beta1";
+                };
             };
         };
     };
