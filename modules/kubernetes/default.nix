c760794bce8cfd132adaba0ac727365f6754ba5a
Bacon <iam@baconi.ng>
2026-02-05 22:14:04 -0500

    fix: i honestly dont even know

--- modules/kubernetes/default.nix
+++ modules/kubernetes/default.nix
@@ -1,23 +1,6 @@
-{ lib, kubenix, ... }:
-
-{ module }:
-
-let
-  normalizedConfig = if builtins.isPath module then import module else module;
-
-  moduleDirs =
-    builtins.filter
-      (n: n != "default.nix")
-      (builtins.attrNames (builtins.readDir ./.));
-
-  modules = map (n: ./${n}) moduleDirs;
-
-  eval = lib.evalModules { 
-    modules = modules ++ [ kubenix.modules.k8s ];
-    specialArgs = { inherit kubenix; };
-    args = { config = normalizedConfig; };
-  };
-in
-{
-  inherit (eval) config options;
+_: {
+    imports = [
+        ./addons
+        ./services
+    ];
 }
