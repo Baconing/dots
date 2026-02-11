8d7330a81263dca34f13014e4634113d69f4c42c
Bacon <iam@baconi.ng>
2026-02-10 20:52:00 -0500

    bruh

---
+++ modules/kubernetes/addons/flannel/default.nix
@@ -0,0 +1,26 @@
+{ lib, kubenix, config, options, ... }:
+let
+    cfg = config.addons.flannel;
+in {
+    imports = [ 
+        kubenix.modules.k8s
+        kubenix.modules.helm
+    ];
+
+    options.addons.flannel = {
+        enable = lib.mkEnableOption "Flannel CNI";
+    };
+
+    config = lib.mkIf cfg.enable {
+        kubernetes = {
+            helm.releases.flannel = {
+                chart = kubenix.lib.helm.fetch {
+                    chartUrl = "https://github.com/flannel-io/flannel/releases/download/v0.28.1/flannel.tgz";
+                    sha256 = "rH3qZRecYtPxlQQz/lumfOT+YHIU8ouEQqBQE3pEPlU=";
+                };
+
+                namespace = "kube-system";
+            };
+        };
+    };
+}
