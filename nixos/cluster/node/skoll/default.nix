b5a6df68387f2f78edd15b6240d475b395a69cd7
Bacon <iam@baconi.ng>
2026-03-25 10:17:05 -0400

    intel gpu

--- nixos/cluster/node/skoll/default.nix
+++ nixos/cluster/node/skoll/default.nix
@@ -1,17 +1,19 @@
-{ config, hostname, inputs, lib, clusterRole, template,  ... }: {
+{ config, hostname, inputs, lib, clusterRole, template, pkgs,  ... }: {
     imports = [
         inputs.nixos-hardware.nixosModules.common-cpu-amd
         inputs.nixos-hardware.nixosModules.common-pc-ssd
     ];
 
-    boot.blacklistedKernelModules = [ "nouveau" ];
-    hardware.nvidia = {
-        open = false;
-        modesetting.enable = true;
-        datacenter.enable = true;
+    hardware.graphics = {
+        enable = true;
+        extraPackages = with pkgs; [
+            intel-media-driver
+            libvdpau-va-gl
+            vpl-gpu-rt
+        ];
     };
-    nixpkgs.config.nvidia.acceptLicense = true;
-    hardware.nvidia-container-toolkit.enable = true;
+
+    environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
 
     homelab.services.kubernetes.netDev = "enp42s0";
 }
