6e878db66d153a74ffa8159bda9c19d6aa3382d0
Bacon <iam@baconi.ng>
2026-09-17 18:52:59 -0400

    add password to user nixos

--- nixos/user.nix
+++ nixos/user.nix
@@ -2,6 +2,20 @@
 {
   environment.localBinInPath = true;
 
+  sops.secrets.root-password = {
+    sopsFile = ./user.secret.yaml;
+    requiredForUsers = true;
+  };
+
+  sops.secrets.bacon-password = {
+    sopsFile = ./user.secret.yaml;
+    requiredForUsers = true;
+  };
+
+  users.users.root = {
+    hashedPasswordFile = config.sops.secrets.root-password.path;
+  };
+
   users.users.bacon = {
     extraGroups = [
       "input"
@@ -11,6 +25,7 @@
     homeMode = "0755";
     isNormalUser = true;
     packages = [ pkgs.home-manager ];
+    hashedPasswordFile = config.sops.secrets.bacon-password.path;
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII9/aKgDkCFCmS2icsuwq17qbZSPwdqwTYQ7pZB4I6qr" ];
     #shell = pkgs.zsh; 
   };
